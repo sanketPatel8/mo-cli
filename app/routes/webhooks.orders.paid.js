f3ea9a2bf436d6eb63e5d31e10325ecd5c914aa2
sanketPatel8 <sanket.xceptive@gmail.com>
2025-08-28 14:19:42 +0530

    fatch order and customer details

--- app/routes/webhooks.orders.paid.js
+++ app/routes/webhooks.orders.paid.js
@@ -1,12 +1,35 @@
 import { json } from "@remix-run/node";
 import { forwardToWebhookSite } from "../utils/forwardToWebhookSite.js";
 
+// export async function action({ request }) {
+//   try {
+//     const shop = request.headers.get("x-shopify-shop-domain");
+//     const payload = await request.json();
+
+//     console.log("💰 Order paid webhook received from Shopify:", payload.id);
+
+//     await forwardToWebhookSite({
+//       url: "https://my-operator.vercel.app/api/shopify/orders",
+//       topic: "orders/paid",
+//       shop,
+//       payload,
+//     });
+
+//     return json({ success: true });
+//   } catch (err) {
+//     console.error("❌ Error handling orders/paid webhook:", err);
+//     return json({ error: "Webhook failed" }, { status: 500 });
+//   }
+// }
+
 export async function action({ request }) {
   try {
+    console.log("📥 Webhook request received");
+
     const shop = request.headers.get("x-shopify-shop-domain");
     const payload = await request.json();
 
-    console.log("💰 Order paid webhook received from Shopify:", payload.id);
+    console.log("💰 Order paid webhook payload:", payload);
 
     await forwardToWebhookSite({
       url: "https://my-operator.vercel.app/api/shopify/orders",

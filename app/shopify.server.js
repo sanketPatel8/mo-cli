e96311ad72c2f60ade7eb860c004fd4fee6ef7d7
sanketPatel8 <sanket.xceptive@gmail.com>
2025-09-02 10:52:56 +0530

    fatch order and customer details

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -91,11 +91,10 @@ const shopify = shopifyApp({
       deliveryMethod: DeliveryMethod.Http,
       callbackUrl: "/webhooks/orders/cancelled",
     },
-    // ORDERS_REFUND: {
-    //   deliveryMethod: DeliveryMethod.Http,
-    //   callbackUrl: "/webhooks/orders/refund",
-    // },
-
+    ORDERS_REFUND: {
+      deliveryMethod: DeliveryMethod.Http,
+      callbackUrl: "/webhooks.orders.refund",
+    },
     APP_UNINSTALLED: {
       deliveryMethod: DeliveryMethod.Http,
       callbackUrl: "/webhooks/app/uninstall",

8a1a02eb7f1b40739ab40d94789ad529020ee4bf
sanketPatel8 <sanket.xceptive@gmail.com>
2025-09-09 18:44:00 +0530

    restore ytday commit

--- app/routes/webhook.fulfillments.update.js
+++ app/routes/webhook.fulfillments.update.js
@@ -1,5 +1,5 @@
 import { json } from "@remix-run/node";
-import { forwardToWebhookSite } from "../../utils/forwardToWebhookSite.js";
+import { forwardToWebhookSite } from "../utils/forwardToWebhookSite";
 
 export async function action({ request }) {
   const topic = request.headers.get("x-shopify-topic"); // should be "fulfillments/update"
